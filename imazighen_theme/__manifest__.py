4a78a259ce390ff74a82fadfb31fc0269dc58e5b
imazighenapps <62152141+imazighenapps@users.noreply.github.com>
2020-03-13 17:56:40 +0100

    Add files via upload

---
+++ imazighen_theme/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Imazighen Theme',
+    'category': 'Themes/Backend',
+    'version': '1.0',
+    'description':
+        """
+Theme Enterprise Web Client.
+===========================
+
+This module modifies the web addon to provide Enterprise design and responsiveness.
+        """,
+    "author": "Imazighen",
+    "price": "69",
+    "currency": "EUR",
+    "support": "imazighenapps@gmail.com",
+    'depends': ['web'],
+    'auto_install': True,
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+   "images": [
+        'static/description/panel.png',
+      
+    ],
+    'qweb': [
+        "static/src/xml/*.xml",
+    ],
+     "license": "OPL-1",
+}
